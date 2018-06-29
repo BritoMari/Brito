c8a59c563b579e6c53de8e6ce0d69d35ad407ac6
Mariana Brito <mariana.bsouza@hotmail.com>
2018-06-29 14:52:35 -0300

    Seção about-me

--- wp-content/themes/brito/front-page.php
+++ wp-content/themes/brito/front-page.php
@@ -24,35 +24,16 @@
 	
 
 	<section class="about-me">
-		<h1 class="titulo11"><?php the_field('titulo_sobre_mim'); ?></h1>
-		<div class="part-noticias">
-		<?php 
-				$destaque = new WP_Query('post_type=post&posts_per_page=4&');
+		<div class="container">
+			<h1 class="titulo1"><?php the_field('titulo_sobre_mim'); ?></h1>
+			<div class="conteudo-wp col-md-7">
+				<?php the_field('conteudo'); ?>
+			</div>
 
-				if($destaque->have_posts()):
-					while($destaque->have_posts()):
-						$destaque->the_post();
-				?>
-						
-					<div class="color">
-						<a href="<?php the_permalink(); ?>">
-							<div class="img-wp" style="background-image:  url(<?php the_field('banner_noticias'); ?>);">
-								<div class="conteudo-wp">
-									<h1 class="titulo-noticia"><?php the_title(); ?></h1>
-									<?php the_excerpt(); ?>
-								</div>
-							</div>
-							<span><i class="fas fa-plus"></i></span>
-						</a>
-					</div>
-						
-				<?php
-					endwhile;
-					wp_reset_postdata();
-				endif;
-
-				 ?>
-				 </div>
+			<div class="imagem col-md-4">
+				<div class="img-wp" style="background-image: url(<?php the_field('my_image'); ?>);"></div>
+			</div>
+		</div>
 	</section>
 	<section class="noticias">
 		<h1 class="titulo11"><?php the_field('titulo_noticias'); ?></h1>
